6b1cf947dd0b88d0fac5e2dd7402758db228c59b
Wesley Fredrickson <wesleyeff@gmail.com>
2016-12-22 11:24:08 -0700

    adding generic config

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  email: {
+    from: '"First Last 👥" <email@host.com>',
+    to: 'email@host.com', // can be comma separated list
+    smtp: 'smtps://email%40gmail.com:password@smtp.gmail.com',
+  },
+  stores: {
+    '123': 'storeName',
+  },
+  iStockUrl: 'http://www.istocknow.com/live/live.php?device.0=airpods',
+};
